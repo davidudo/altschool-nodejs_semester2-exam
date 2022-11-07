2652fc9427f030884fb16a88544d09eb51ecc380
davidudo <udodavid46.ud@gmail.com>
2022-11-07 19:19:21 +0100

    Created a simple documentation web page

--- index.js
+++ index.js
@@ -28,15 +28,14 @@ app.use(morgan('tiny'));
 // Connect to MongoDB database
 connectToMongoDB();
 
+app.use('/auth', authRouter);
+app.use('/user', userRouter);
+app.use('/blog', blogRouter);
 
 // Routes
 app.get('/', (req, res) => {
   res.send('Welcome to Altschool Blog API');
 });
-
-app.use('/auth', authRouter);
-app.use('/user', userRouter);
-app.use('/blog', blogRouter);
 
 // 404
 app.use((req, res, next) => {
